338ef349c9b6b172bd06be8fc31557b3d57f28da
vcu-dickinsonaw <78313938+vcu-dickinsonaw@users.noreply.github.com>
2021-03-08 12:31:05 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -2,33 +2,38 @@
 
 
 def find_greatest_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the largest number in the list.
-    """
+    incoming_list = [2, 4, 6, 8, 10]
+    find_greatest_number(max(incoming_list))
     pass
 
 
 def find_least_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the smallest/least number in the list.
-    """
+    incoming_list = [2, 4, 6, 8, 10]
+    find_least_number(min(incoming_list))
     pass
 
 
 def add_list_numbers(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
-    """
-
+    incoming_list = [2, 4, 6, 8, 10]
+    add_list_numbers(sum(incoming_list))
     pass
 
 
 def longest_value_key(incoming_dict):
-    """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
-    """
+    incoming_dict = {
+        "name": "Alex Dickinson",
+        "study": "Information Systems",
+        "college": "VCU"
+    }
+    first_key_length = len(incoming_dict['name'])
+    second_key_length = len(incoming_dict['study'])
+    third_key_length = len(incoming_dict['college'])
+
+    if first_key_length > second_key_length and first_key_length > third_key_length:
+        longest_value_key(first_key_length)
+    elif second_key_length > first_key_length and second_key_length > third_key_length:
+        longest_value_key(second_key_length)
+    else:
+        longest_value_key(third_key_length)
+
     pass
