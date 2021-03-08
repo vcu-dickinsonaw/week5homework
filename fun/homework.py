cfbb181bc042867ac65e2ed0f0927ab2e70ddb18
vcu-dickinsonaw <78313938+vcu-dickinsonaw@users.noreply.github.com>
2021-03-08 13:02:24 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -1,30 +1,33 @@
 """ Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
     incoming_list = [2, 4, 6, 8, 10]
     find_greatest_number(max(incoming_list))
     pass
+
 
 def find_least_number(incoming_list):
     incoming_list = [2, 4, 6, 8, 10]
     find_least_number(min(incoming_list))
     pass
 
+
 def add_list_numbers(incoming_list):
     incoming_list = [2, 4, 6, 8, 10]
     add_list_numbers(sum(incoming_list))
     pass
 
+
 def longest_value_key(incoming_dict):
     incoming_dict = {
         "name": "Alex Dickinson",
         "study": "Information Systems",
-        "college": "VCU"
+        "college": "VCU",
     }
-    
-    first_key_length = len(incoming_dict['name'])
-    second_key_length = len(incoming_dict['study'])
-    third_key_length = len(incoming_dict['college'])
+    first_key_length = len(incoming_dict["name"])
+    second_key_length = len(incoming_dict["study"])
+    third_key_length = len(incoming_dict["college"])
 
     if first_key_length > second_key_length and first_key_length > third_key_length:
         longest_value_key(first_key_length)
