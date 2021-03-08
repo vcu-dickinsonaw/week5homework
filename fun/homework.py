6f737a21839d7ad6360b655b7454dd966a088852
vcu-dickinsonaw <78313938+vcu-dickinsonaw@users.noreply.github.com>
2021-03-08 15:19:57 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -12,7 +12,7 @@ def find_least_number(incoming_list):
 
 
 def add_list_numbers(incoming_list):
-    return sum(filter(None, incoming_list))
+    return sum(filter(None, incoming_list, 0))
     pass
 
 
