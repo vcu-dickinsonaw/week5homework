553e10277f9e620541a8d842dc5dd1877f72783e
vcu-dickinsonaw <78313938+vcu-dickinsonaw@users.noreply.github.com>
2021-03-08 14:30:55 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -2,38 +2,27 @@
 
 
 def find_greatest_number(incoming_list):
-    incoming_list = [2, 4, 6, 8, 10]
-    find_greatest_number(max(incoming_list))
+    max(incoming_list)
     pass
 
 
 def find_least_number(incoming_list):
-    incoming_list = [2, 4, 6, 8, 10]
-    find_least_number(min(incoming_list))
+    min(incoming_list)
     pass
 
 
 def add_list_numbers(incoming_list):
-    incoming_list = [2, 4, 6, 8, 10]
-    add_list_numbers(sum(incoming_list))
+    sum(incoming_list)
     pass
 
 
 def longest_value_key(incoming_dict):
-    incoming_dict = {
-        "name": "Alex Dickinson",
-        "study": "Information Systems",
-        "college": "VCU",
-    }
-    first_key_length = len(incoming_dict["name"])
-    second_key_length = len(incoming_dict["study"])
-    third_key_length = len(incoming_dict["college"])
+    first_key_length = len(incoming_dict[0:0])
+    second_key_length = len(incoming_dict[1:0])
 
-    if first_key_length > second_key_length and first_key_length > third_key_length:
+    if first_key_length > second_key_length:
         longest_value_key(first_key_length)
-    elif second_key_length > first_key_length and second_key_length > third_key_length:
-        longest_value_key(second_key_length)
     else:
-        longest_value_key(third_key_length)
+        longest_value_key(second_key_length)
 
     pass
